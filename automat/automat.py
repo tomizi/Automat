8e94dde4e9b3ab5a0cf12ac306dd48677048600b
tomizi <106354750+tomizi@users.noreply.github.com>
2022-07-10 17:13:00 +0200

    Update automat.py

--- automat/automat.py
+++ automat/automat.py
@@ -16,8 +16,8 @@ st.title('Wszystkie pliki')
 
 st.sidebar.subheader('Dodawanie plików')
 
-uploaded_file = st.sidebar.file_uploader(label='Wprwadź pliki', type=['xlsx'],accept_multiple_files=True)
-uploaded_file1 = st.sidebar.file_uploader(label='Wprwadź plik z ostatniego miesiąca', type=['xlsx'])
+uploaded_file = st.sidebar.file_uploader(label='Wprowadź pliki', type=['xlsx'],accept_multiple_files=True)
+uploaded_file1 = st.sidebar.file_uploader(label='Wprowadź plik z ostatniego miesiąca', type=['xlsx'])
 
 
 global df, l, zl, df1, z1
